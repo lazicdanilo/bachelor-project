ef5416d307720b3aedf2e7570dcd98b3eb454580
Danilo <lazicdanilo47@gmail.com>
2021-08-15 18:43:40 +0200

    Added lift Added two more suction cups All packs now have Conections and can be picked by suction cup

--- webots/controllers/my_controller/my_controller.c
+++ webots/controllers/my_controller/my_controller.c
@@ -14,16 +14,23 @@ int main(int argc, char **argv) {
  // get the motor devices
  WbDeviceTag left_motor = wb_robot_get_device("wheel_left");
  WbDeviceTag right_motor = wb_robot_get_device("wheel_right");
+ WbDeviceTag lift_c = wb_robot_get_device("lift_center");
+ WbDeviceTag lift_r = wb_robot_get_device("lift_right");
+ WbDeviceTag lift_l = wb_robot_get_device("lift_left");
  // set the target position of the motors
  
  WbDeviceTag ds_l = wb_robot_get_device("distance_sens_left");
  WbDeviceTag ds_r= wb_robot_get_device("distance_sens_right");
 
   // WbDeviceTag puck_conn = wb_robot_get_device("connector_pp");
-  WbDeviceTag vac_conn = wb_robot_get_device("connector_v");
-  wb_connector_enable_presence(vac_conn, 1);
+  WbDeviceTag vac_conn_c = wb_robot_get_device("vacuum_connector_center");
+  WbDeviceTag vac_conn_r = wb_robot_get_device("vacuum_connector_right");
+  WbDeviceTag vac_conn_l = wb_robot_get_device("vacuum_connector_left");
+  wb_connector_enable_presence(vac_conn_c, 1);
+  wb_connector_enable_presence(vac_conn_r, 1);
+  wb_connector_enable_presence(vac_conn_l, 1);
   
-  printf("pres = %d\n", wb_connector_get_presence(vac_conn));
+  // printf("pres = %d\n", wb_connector_get_presence(vac_conn));
   // printf("presssss");
   
    
@@ -37,25 +44,48 @@ int main(int argc, char **argv) {
    // printf("ds l = %f\n", ds_l_val);
    // printf("ds l = %f\n", ds_r_val);
    // printf("pres = %d\n", wb_connector_get_presence(vac_conn));
-   if (!wb_connector_is_locked(vac_conn)){
-     if (wb_connector_get_presence(vac_conn) == 1)
+   if (!wb_connector_is_locked(vac_conn_c)){
+     if (wb_connector_get_presence(vac_conn_c) == 1)
      {
-       wb_connector_lock(vac_conn);
+       wb_connector_lock(vac_conn_c);
        printf("LOCKED\n");
      }
    }
-   if (counter == 10)
+   
+      if (!wb_connector_is_locked(vac_conn_r)){
+     if (wb_connector_get_presence(vac_conn_r) == 1)
+     {
+       wb_connector_lock(vac_conn_r);
+       printf("LOCKED\n");
+     }
+   }
+   
+      if (!wb_connector_is_locked(vac_conn_l)){
+     if (wb_connector_get_presence(vac_conn_l) == 1)
+     {
+       wb_connector_lock(vac_conn_l);
+       printf("LOCKED\n");
+     }
+   }
+   if (counter == 1)
+   {
+     wb_motor_set_position(lift_c, 0);
+     wb_motor_set_position(lift_l, 0);
+     wb_motor_set_position(lift_r, 0);
+   }
+   if (counter == 200)
    {
    
 
- wb_motor_set_position(left_motor, 5.1);
- wb_motor_set_position(right_motor, 5.1);
+ wb_motor_set_position(left_motor, 5.05);
+ wb_motor_set_position(right_motor, 5.05);
       // wb_motor_set_position(left_motor, 30.0);
       
    }
    
-   else if (counter == 350)
+   else if (counter == 550)
    {
+   
    // if (ds_l_val < 1000.0)
    // {
      wb_motor_set_position(left_motor, -5.0);
@@ -68,6 +98,14 @@ int main(int argc, char **argv) {
  // wb_motor_set_position(left_motor, 30.0);
  // wb_motor_set_position(right_motor, 30.0);
    }
+      else if (counter == 950)
+   {
+     wb_motor_set_position(lift_c, 0.1);
+     wb_motor_set_position(lift_l, 0.1);
+     wb_motor_set_position(lift_r, 0.1);
+   }
+   
+   
    
       // else if (counter == 1100)
    // {
